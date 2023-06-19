186207bcbb74311211c5e221d2ab5adb08c44682
JayaBelagurki <jayabcs17@gmail.com>
2023-06-19 15:31:00 -0500

    event binding in angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  name:string="India";
+  title:string = 'bankingapp-ui';
+  message:string="";
+
+  public auth(userInput:any,passwordInput:any):void {
+    if("jack"===userInput.value && "jill"===passwordInput.value){
+          this.message="Hey! username and password are correct!";
+    }else{
+      this.message="Sorry! username and password are not correct!";
+    }
+  }
+
+}
